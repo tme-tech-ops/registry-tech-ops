fe01f10853da6af2427b8acead09d894ff80a596
Chubtoad <rhfmeyer@gmail.com>
2026-02-24 16:15:57 -0500

    first pass
    harbor integration public git

---
+++ tech-ops/scripts/env_precheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ $OFFLINE_MODE == "true" ]]; then
+    echo "Offline mode detected."
+    if [[ -z $OFFLINE_BINARY_URL || -z $OFFLINE_BINARY_USER || -z $OFFLINE_BINARY_PASSWORD ]]; then
+        echo "OFFLINE_BINARY_URL, OFFLINE_BINARY_USER, or OFFLINE_BINARY_PASSWORD is missing."
+        exit 1
+    else
+        echo "Downloading offline binary..."
+        curl -kOLu $OFFLINE_BINARY_USER:$OFFLINE_BINARY_PASSWORD $OFFLINE_BINARY_URL
+        if [[ $? -ne 0 ]]; then 
+            echo "Failed to download offline binary."
+            exit 1
+        fi
+        echo "Extracting offline binary..."
+        tar -xzf $(basename $OFFLINE_BINARY_URL)
+        if [[ $? -ne 0 ]]; then 
+            echo "Failed to extract offline binary."
+            exit 1
+        fi
+    fi
+else
+    echo "Online mode detected."
+    if [[ -z $SCRIPT_URL ]]; then
+        echo "SCRIPT_URL is missing."
+        exit 1
+    fi
+    curl -kOL $SCRIPT_URL
+    if [[ $? -ne 0 ]]; then 
+        echo "Failed to download script."
+        exit 1
+    fi
+    chmod +x $(basename $SCRIPT_URL)
+fi

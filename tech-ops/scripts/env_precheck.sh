52b080d5e075c4920acd46674f91567c39f4cec6
Chubtoad <rhfmeyer@gmail.com>
2026-02-24 22:00:23 -0500

    changed download directory from tmp

--- tech-ops/scripts/env_precheck.sh
+++ tech-ops/scripts/env_precheck.sh
@@ -7,7 +7,7 @@ if [[ $OFFLINE_MODE == "true" ]]; then
         exit 1
     else
         echo "Downloading offline binary..."
-        curl -kOLu $OFFLINE_BINARY_USER:$OFFLINE_BINARY_PASSWORD $OFFLINE_BINARY_URL
+        curl -sfLu $OFFLINE_BINARY_USER:$OFFLINE_BINARY_PASSWORD $OFFLINE_BINARY_URL -o ~/$(basename $OFFLINE_BINARY_URL)
         if [[ $? -ne 0 ]]; then 
             echo "Failed to download offline binary."
             exit 1
@@ -25,10 +25,10 @@ else
         echo "SCRIPT_URL is missing."
         exit 1
     fi
-    curl -kOL $SCRIPT_URL
+    curl -sfL $SCRIPT_URL -o ~/$(basename $SCRIPT_URL)
     if [[ $? -ne 0 ]]; then 
         echo "Failed to download script."
         exit 1
     fi
-    chmod +x $(basename $SCRIPT_URL)
+    chmod +x ~/$(basename $SCRIPT_URL)
 fi

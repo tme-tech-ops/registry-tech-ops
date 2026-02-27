dc7a9af76d26f2d976b8eda504a71e677d32ffa8
Chubtoad <rhfmeyer@gmail.com>
2026-02-27 12:13:41 -0500

    added install modes

--- tech-ops/scripts/env_precheck.sh
+++ tech-ops/scripts/env_precheck.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 if [[ $OFFLINE_MODE == "true" ]]; then
-    echo "Offline mode detected."
+    ctx logger info "Offline mode detected."
     if [[ -z $OFFLINE_BINARY_URL || -z $OFFLINE_BINARY_USER || -z $OFFLINE_BINARY_PASSWORD ]]; then
-        echo "OFFLINE_BINARY_URL, OFFLINE_BINARY_USER, or OFFLINE_BINARY_PASSWORD is missing."
+        ctx logger info "OFFLINE_BINARY_URL, OFFLINE_BINARY_USER, or OFFLINE_BINARY_PASSWORD is missing."
         exit 1
     else
-        echo "Downloading offline binary..."
+        ctx logger info "Downloading offline binary..."
         curl -sfLu $OFFLINE_BINARY_USER:$OFFLINE_BINARY_PASSWORD $OFFLINE_BINARY_URL -o ~/$(basename $OFFLINE_BINARY_URL)
         if [[ $? -ne 0 ]]; then 
-            echo "Failed to download offline binary."
+            ctx logger info "Failed to download offline binary."
             exit 1
         fi
-        echo "Extracting offline binary..."
+        ctx logger info "Extracting offline binary..."
         tar -xzf $(basename $OFFLINE_BINARY_URL) -C ~/
         if [[ $? -ne 0 ]]; then 
-            echo "Failed to extract offline binary."
+            ctx logger info "Failed to extract offline binary."
             exit 1
         fi
     fi
 else
-    echo "Online mode detected."
+    ctx logger info "Online mode detected."
     if [[ -z $SCRIPT_URL ]]; then
-        echo "SCRIPT_URL is missing."
+        ctx logger info "SCRIPT_URL is missing."
         exit 1
     fi
     curl -sfL $SCRIPT_URL -o ~/$(basename $SCRIPT_URL)
     if [[ $? -ne 0 ]]; then 
-        echo "Failed to download script."
+        ctx logger info "Failed to download script."
         exit 1
     fi
     chmod +x ~/$(basename $SCRIPT_URL)
